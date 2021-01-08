abf58cf1f36ef3f8d0192bff7c4b618e1293dcd0
proflucascastro <lucasfcastro.prof@gmail.com>
2021-01-08 19:31:21 -0300

    fix bug - colisao chao

--- js/chao.js
+++ js/chao.js
@@ -12,24 +12,11 @@ class Chao extends Sprite {
     atualizar() {
         if (this.rolagem == 'horizontal') {
             this.x -= this.velocidadeX;
-            if (this.x < -this.imagem.width) this.x = 0;
+            if (this.x < -this.imagem.width) this.x = this.animacao.contexto.canvas.width;
         } else {
             this.y -= this.velocidadeY;
             if (this.y < -this.imagem.height) this.y = 0;
         }
     }
 
-    desenhar() {
-        const h = Math.floor(this.animacao.contexto.canvas.width/this.imagem.width)+1
-        const v = Math.floor(this.animacao.contexto.canvas.height/this.imagem.height)+1
-        if (this.rolagem == 'horizontal') {
-            for (let i = 0; i <= h; i++) {
-                this.animacao.contexto.drawImage(this.imagem, this.x + i * this.imagem.width, this.y);
-            }
-        } else {
-            for (let i = 0; i <= h; i++) {
-                this.animacao.contexto.drawImage(this.imagem, this.x, this.y + i * this.imagem.height);
-            }
-        }
-    }
 }
